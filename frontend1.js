aa5d2400d7f25dde63f9911c63a84d0f12e9e385
Mohamed Ahmed <100856846+Mohamed20a@users.noreply.github.com>
2022-05-31 16:02:39 +0200

    Add files via upload

---
+++ frontend1.js
@@ -0,0 +1,16 @@
+var class1 = " Welcome to my Form ";
+alert(class1).css({"fontsize":"20px","color":"red"});
+
+var time = new Data()
+console.log(time);
+
+
+$(function(){
+    $("#btn").click(function(){
+        $("#h1").animate(1000);
+    });
+
+    //$("#h1").hover(function(){
+      //  $(this).animate({fontSize:'60px',opacity:'1'},2000)
+    //})
+});
